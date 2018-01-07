96a63f0e2721f2362db7882ee5b8321a206bda95
my pc <my pc@DESKTOP-V1AG363>
2018-01-07 13:18:54 +0530

    new sop added23

--- src/First.java
+++ src/First.java
@@ -4,6 +4,7 @@ public class First {
 	public static void main(String[] args) {
 	System.out.println("rave welcom java");
 	System.out.println("rave welcom java");
+	System.out.println("rave welcom java");
     
                  
 	
